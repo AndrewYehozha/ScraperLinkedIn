16a42c8e25bb97cf528592b3b3787ef940a16b84
Andrew <andrew.yehozha@gmail.com>
2019-08-30 21:42:42 +0300

    Добавьте файлы проекта.

---
+++ ScraperLinkedIn/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScraperLinkedIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scraper = new Scraper();
+            scraper.Initialize();
+            Environment.Exit(0);
+        }
+    }
+}
